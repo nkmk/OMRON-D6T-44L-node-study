bd9ab887ef8020c943c5d3f184d7f83820f9eefc
nkmk <nkmk.on@gmail.com>
2014-08-27 17:25:34 +0900

    add reference links

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'D6T-44L-06' });
+  res.render('index', { title: 'D6T-44L' });
 });
 
 module.exports = router;
